b3ce365a836ea03454673de0f3b33cd5eb468c2b
appdrew <59877178+appdrew@users.noreply.github.com>
2020-01-14 10:11:00 -0500

    Create Beggining.py

---
+++ Beggining.py
@@ -0,0 +1,5 @@
+# = It's a comment. When you put # in front your string line text became a "comment".
+
+import sys # Importing extra modules with "import" 
+
+print('Welcome to appDrew') # Will be printing in display the text from quotes ('')
